f11034eaf1b068156f15f86dff3ac04ade1b4c13
DavonnEscobilla <qdpescobilla@tip.edu.ph>
2024-04-10 16:14:51 +0800

    Added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Projects from './pages/Projects';
 import Header from './components/Header';
 import SignUp from './pages/Signup';
 import SignIn from './pages/SignIn';
+import Footer from './components/Footer';
 
 
 export default function App() {
@@ -23,6 +24,7 @@ export default function App() {
           <Route path='/projects' element={<Projects />} />
 
         </Routes>
+        <Footer />
 
       </BrowserRouter>
     )
